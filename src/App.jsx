0e18f2fa93e119ee02d9d53eb82599cc578f2d4d
Kieran <kierantahir@gmail.com>
2023-08-10 12:47:34 +1200

    state and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import './styles/index.scss'
+import "./styles/index.scss";
+import NavBar from "./NavBar";
+import Fish from "./Fish";
+import Bird from "./Bird";
+import Insect from "./Insect";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const [currentTab, setCurrentTab] = useState("Fish");
+
   return (
     <div className="App">
-      Leshgo v1
+      <div className="row">
+        <NavBar setCurrentTab={setCurrentTab} />
+        <div className="column">
+          {currentTab === "Fish" && <Fish />}
+          {currentTab === "Bird" && <Bird />}
+          {currentTab === "Insect" && <Insect />}
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Insect.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Insect() {
+  return (
+    <>
+      <img src="/weta.webp" alt="Weta" className="image"/>
+    </>
+  );
+}
+
+export default Insect
